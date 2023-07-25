a5d041ea140733429a982f2fde5b227c11681d00
Laura Machlab <92225887+lmachlab@users.noreply.github.com>
2023-07-25 11:25:58 -0500

    fixing errors

--- Colonoscopy_resize_training.py
+++ Colonoscopy_resize_training.py
@@ -31,37 +31,6 @@ def displayTensor(input_img: torch.tensor, file_name) -> None:
     input_img_cpu = np.squeeze(input_img_cpu)
     plt.imsave(file_name,input_img_cpu, cmap='gray')
 
-class BCEDiceLoss(nn.Module):
-    def __init__(self, weight=None, size_average=True):
-        super().__init__()
-
-    def forward(self, input, target):
-        pred = input.view(-1)
-        truth = target.view(-1)
-
-        # BCE loss
-        bce_loss = nn.BCELoss()(pred, truth).double()
-
-        # Dice Loss
-        dice_coef = (2.0 * (pred * truth).double().sum() + 1) / (
-            pred.double().sum() + truth.double().sum() + 1
-        )
-
-        return bce_loss + (1 - dice_coef)
-
-def dice_coeff(input, target):
-    num_in_target = input.size(0)
-
-    smooth = 1.0
-
-    pred = input.view(num_in_target, -1)
-    truth = target.view(num_in_target, -1)
-
-    intersection = (pred * truth).sum(1)
-
-    loss = (2.0 * intersection + smooth) / (pred.sum(1) + truth.sum(1) + smooth)
-
-    return loss.mean().item()
 
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -93,7 +62,8 @@ if __name__ == "__main__":
     valid_mask_paths.sort()
     
     ## Parameters
-    image_size = 256
+    image_h = 496
+    image_w = 768
     batch_size = 8
     lr = 1e-4
     epochs = 200
@@ -103,8 +73,8 @@ if __name__ == "__main__":
     valid_steps = len(valid_image_paths)//batch_size
     print("valid steps: ", valid_steps)
     
-    train_gen = DataGen(image_size, train_image_paths, train_mask_paths)
-    valid_gen = DataGen(image_size, valid_image_paths, valid_mask_paths)
+    train_gen = DataGen(image_h, image_w, train_image_paths, train_mask_paths, noise=True)
+    valid_gen = DataGen(image_h, image_w, valid_image_paths, valid_mask_paths, noise=True)
     
     ## Turn the data into a torch.utils.data thing
     train_loader = torch.utils.data.DataLoader(train_gen, batch_size=8)
